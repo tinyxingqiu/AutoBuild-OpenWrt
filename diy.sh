08920db236cceaafe4064744b938e69d561819ce
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2020-11-19 19:55:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,7 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: Vip openwrt
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
