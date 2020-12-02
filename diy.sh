820cb5519d08676defa2d980f631ac443d8ef2e7
TinyXQ <50978550+tinyxingqiu@users.noreply.github.com>
2020-12-02 10:37:27 +0800

    Delete diy.sh

--- diy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: Vip openwrt
-#=================================================
-# Modify default IP
-# sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
